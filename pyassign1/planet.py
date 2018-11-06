7b26f416c03818346f2783f4b40d67581bb4096c
李硕学 <1620480103@qq.com>
2018-11-06 23:38:42 +0800

    Update planet.py

--- pyassign1/planet.py
+++ pyassign1/planet.py
@@ -47,8 +47,8 @@ def run():
     k=1
     while (1):
         for x in range(6):
-            planet[x].goto(a[x]*math.cos(2*k/(a[x]**1.5))+c(x),\
-                            b(x)*math.sin(2*k/(a[x]**1.5)))
+            planet[x].goto(a[x]*math.cos(10*k/(a[x]**1.5))+c(x),\
+                            b(x)*math.sin(10*k/(a[x]**1.5)))
             k=k+1
 
 def main():
